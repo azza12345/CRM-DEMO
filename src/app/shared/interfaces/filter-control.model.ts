ab131e9aab93a37f01e72b27bb5a6b97139ac13a
mohamed_ahmed <mohamed.hussein@sewedy.com.eg>
2024-12-24 16:23:25 +0200

    Add Adaptive Filter and Table components

---
+++ src/app/shared/interfaces/filter-control.model.ts
@@ -0,0 +1,13 @@
+import { ValidatorFn } from '@angular/forms';
+import { EndPoint } from '@shared/enums';
+
+export interface FilterControl {
+  formControlName: string;
+  label: string;
+  type: 'text' | 'select' | 'autocomplete' | 'date' | 'checkbox' | 'radio';
+  options?: Array<{ value: any; label: string }>;
+  validators?: Array<ValidatorFn>;
+  apiEndpoint?: EndPoint;
+  key?: string; //if the data for example not coming as an array of string like array of objects , we have to know each value then
+  initialValue?: any;
+}
