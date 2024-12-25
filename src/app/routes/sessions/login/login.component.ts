7e02e737d0954c2d7e6faf89af3cbda96aff56fd
mohamed_ahmed <mohamed.hussein@sewedy.com.eg>
2024-12-25 11:11:28 +0200

    clear controls

--- src/app/routes/sessions/login/login.component.ts
+++ src/app/routes/sessions/login/login.component.ts
@@ -37,8 +37,8 @@ export class LoginComponent {
   isSubmitting = false;
 
   loginForm = this.fb.nonNullable.group({
-    username: ['eng.mhd.ahmed@gmail.com', [Validators.required]],
-    password: ['P@$$w0rd', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
   });
 
   constructor(
