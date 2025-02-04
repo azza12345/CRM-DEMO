56920a3615b1f3b4ade20d848e83d203fa9ce63b
mohamed_ahmed <mohamed.hussein@sewedy.com.eg>
2025-02-04 13:57:23 +0200

    fix: api meter stat naming

--- src/app/shared/enums.ts
+++ src/app/shared/enums.ts
@@ -13,7 +13,7 @@ export enum EndPoint {
   GET_AGENTS_BY_DISTRICT_ID = 'Districts/Contractors',
   RETIRED_METERS = 'Meters/retired',
   DISTRICTS = 'Districts/all',
-  METERS_STATISTICS = 'meters/statistics/district',
+  METERS_STATISTICS = 'meters/statistics',
   RETIRED_METER_STATISTICS = 'meters/retired/status/district',
   AGENTS_OPERATIONS_STATISTICS = 'agents/operations/statistics',
   //FIXME: gonna be changed when API is Ready
