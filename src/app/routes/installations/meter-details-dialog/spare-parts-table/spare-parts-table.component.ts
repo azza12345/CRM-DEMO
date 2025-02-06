f78214eb0cfd707eb98a0a3cbbc6fc04cd1fa9d0
mohamed_ahmed <mohamed.hussein@sewedy.com.eg>
2025-02-06 13:26:49 +0200

    feat: spare parts table logic

--- src/app/routes/installations/meter-details-dialog/spare-parts-table/spare-parts-table.component.ts
+++ src/app/routes/installations/meter-details-dialog/spare-parts-table/spare-parts-table.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MeterItem } from '@shared/interfaces/meter-info.model';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-spare-parts-table',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, MatTableModule],
   templateUrl: './spare-parts-table.component.html',
-  styleUrl: './spare-parts-table.component.scss'
+  styleUrl: './spare-parts-table.component.scss',
 })
 export class SparePartsTableComponent {
+  private _spareParts$: Observable<MeterItem[]> = of([]);
 
+  @Input() title!: string;
+
+  @Input() set spareParts(value: MeterItem[] | null) {
+    this._spareParts$ = of(value || []);
+  }
+
+  get spareParts$(): Observable<MeterItem[]> {
+    return this._spareParts$;
+  }
 }
