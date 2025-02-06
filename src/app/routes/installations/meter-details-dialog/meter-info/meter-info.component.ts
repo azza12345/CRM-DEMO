93eb5093369fde42beb292aba64bac794ce38903
mohamed_ahmed <mohamed.hussein@sewedy.com.eg>
2025-02-06 13:15:33 +0200

    feat: meter details component

--- src/app/routes/installations/meter-details-dialog/meter-info/meter-info.component.ts
+++ src/app/routes/installations/meter-details-dialog/meter-info/meter-info.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+export interface MeterDetail {
+  label: string;
+  value: string | number | null;
+}
 @Component({
   selector: 'app-meter-info',
   standalone: true,
   imports: [],
   templateUrl: './meter-info.component.html',
-  styleUrl: './meter-info.component.scss'
+  styleUrl: './meter-info.component.scss',
 })
 export class MeterInfoComponent {
-
+  @Input() details: MeterDetail[] = [];
 }
