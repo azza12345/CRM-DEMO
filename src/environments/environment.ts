69b046057cf2d8ef1c24f5779fdea805ed145f7e
Azza 20210567 <Azza_20210567@fci.helwan.edu.eg>
2025-02-16 13:56:25 +0200

    fix(30312): update search function on agent list page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // ApiUrl: 'assets',
   // ApiUrl: 'http://192.168.5.48:8070/api/v1',
-  ApiUrl: 'https://localhost:7130/api/v1',
+  ApiUrl: 'http://localhost:5218/api/v1',
   baseUrl: '',
   useHash: false,
   releaseDate: '2025',
